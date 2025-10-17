6ffb456b4b5a973fc9007a5abced1bf74cc7e537
Aleksei Statkevich <astatkevich@x.ai>
2025-10-17 10:29:58 -0700

    Resolve the problem with duplicate connect methods

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
+        .build_transport(false)
         .out_dir("src/proto")
         .compile_protos(
             &[
